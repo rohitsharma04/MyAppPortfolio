59be2a95063fab0025fe29f5aa1a4d58f1983b5f
Rohit Sharma <skyrohithigh@gmail.com>
2016-04-01 18:04:42 +0530

    Used ButterKnife

--- app/src/main/java/com/bitshifters/rohit/myappportfolio/MainActivity.java
+++ app/src/main/java/com/bitshifters/rohit/myappportfolio/MainActivity.java
@@ -1,6 +1,5 @@
 package com.bitshifters.rohit.myappportfolio;
 
-import android.content.Context;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,77 +8,64 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
 
     private final String TAG = MainActivity.class.getSimpleName();
-    private Button spotifyStreamer, scoresApp, libraryApp, buildItBiggerApp, xyzReaderApp, capstoneMyOwnApp;
-    private Context context;
-    private Resources resources;
+
+    @Bind(R.id.btn_spotify_streamer_app) protected Button mSpofityStreamer;
+    @Bind(R.id.btn_scores_app) protected Button mScores;
+    @Bind(R.id.btn_library_app) protected Button mLibrary;
+    @Bind(R.id.btn_build_it_bigger) protected Button mBuildItBigger;
+    @Bind(R.id.btn_xyz_reader_app) protected Button mXYZReader;
+    @Bind(R.id.btn_my_own_app) protected Button mCapstone;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.v(TAG, "onCreate Method");
         setContentView(R.layout.activity_main);
-        //Calling initializeEverything Method
-        initializeEverything();
+
+        //Binding all views
+        ButterKnife.bind(this);
     }
 
-    private void initializeEverything(){
-        Log.v(TAG,"initializeEverything Method");
-
-        //Setting up context and resources
-        context = getApplicationContext();
-        resources = getResources();
-
-        //Initializing all buttons
-        spotifyStreamer = (Button) findViewById(R.id.btn_spotify_streamer_app);
-        scoresApp = (Button) findViewById(R.id.btn_scores_app);
-        libraryApp = (Button) findViewById(R.id.btn_library_app);
-        buildItBiggerApp = (Button) findViewById(R.id.btn_build_it_bigger);
-        xyzReaderApp = (Button) findViewById(R.id.btn_xyz_reader_app);
-        capstoneMyOwnApp = (Button) findViewById(R.id.btn_my_own_app);
-
-        //Setting up OnClickListers for all the buttons
-        spotifyStreamer.setOnClickListener(this);
-        scoresApp.setOnClickListener(this);
-        libraryApp.setOnClickListener(this);
-        buildItBiggerApp.setOnClickListener(this);
-        xyzReaderApp.setOnClickListener(this);
-        capstoneMyOwnApp.setOnClickListener(this);
-    }
-
-    //Click Events
-    @Override
-    public void onClick(View v) {
-        Log.v(TAG, "onClick Method");
+    @OnClick({R.id.btn_spotify_streamer_app, R.id.btn_scores_app, R.id.btn_library_app,
+            R.id.btn_build_it_bigger, R.id.btn_xyz_reader_app, R.id.btn_my_own_app})
+    protected void onClick(View v){
+        Log.v(TAG,"onClick");
+        Resources resources = getResources();
         String commonText = "This button will launch my ";
         String toastText;
         switch (v.getId()){
             case R.id.btn_spotify_streamer_app:
                 toastText = commonText + resources.getString(R.string.spotify_streamer_app);
-                Toast.makeText(context,toastText,Toast.LENGTH_LONG).show();
+                Toast.makeText(this,toastText,Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_scores_app:
                 toastText = commonText + resources.getString(R.string.scores_app);
-                Toast.makeText(context,toastText,Toast.LENGTH_LONG).show();
+                Toast.makeText(this,toastText,Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_library_app:
                 toastText = commonText + resources.getString(R.string.library_app);
-                Toast.makeText(context,toastText,Toast.LENGTH_LONG).show();
+                Toast.makeText(this,toastText,Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_build_it_bigger:
                 toastText = commonText + resources.getString(R.string.build_it_bigger_app);
-                Toast.makeText(context,toastText,Toast.LENGTH_LONG).show();
+                Toast.makeText(this,toastText,Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_xyz_reader_app:
                 toastText = commonText + resources.getString(R.string.xyz_reader_app);
-                Toast.makeText(context,toastText,Toast.LENGTH_LONG).show();
+                Toast.makeText(this,toastText,Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_my_own_app:
                 toastText = commonText + resources.getString(R.string.my_own_app);
-                Toast.makeText(context,toastText,Toast.LENGTH_LONG).show();
+                Toast.makeText(this,toastText,Toast.LENGTH_SHORT).show();
                 break;
         }
     }
+
 }
